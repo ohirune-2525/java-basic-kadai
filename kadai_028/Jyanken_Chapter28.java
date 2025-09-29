17f66da9787ef397099636a2d8a5915a0e618fec
ohirune-2525 <chiyuki.frst@gmail.com>
2025-09-29 20:41:58 +0900

    kadai_028をアップロード

---
+++ kadai_028/Jyanken_Chapter28.java
@@ -0,0 +1,74 @@
+package kadai_028;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Jyanken_Chapter28 {
+	String myHand = "";//空文字
+	String yourHand = "";
+	String outcome;
+	public String getMyChoice() {
+		//入力を受け取るところまで
+		  System.out.println("自分のじゃんけんの手を入力しましょう");
+		  System.out.println("グーはrockのrを入力しましょう");
+		  System.out.println("チョキはscissorsのsを入力しましょう");
+		  System.out.println("パーはpaperのpを入力しましょう");
+	      Scanner Jyanken_Chapter28_scanner = new Scanner(System.in);
+	      String input= Jyanken_Chapter28_scanner.next();
+	      
+	      Jyanken_Chapter28_scanner.close();
+	      
+	      //入力値の判断
+	      switch(input) {
+	      	case "r" -> myHand = "r";
+	      	case "s" -> myHand = "s";
+	      	case "p" -> myHand = "p";
+	      	default -> System.out.println("正しい手を入力してください");
+	      }
+	      //myHandの値を返す
+	      return myHand;
+	}
+	
+	public String getRandom() {
+		int yourHandIndex;
+		
+		ArrayList<String> jyankenList = new ArrayList<String>();
+		jyankenList.add("r");
+		jyankenList.add("s");
+		jyankenList.add("p");
+		
+		//0から2までの乱数を出力して、値をyourHandIndexに代入したい
+		yourHandIndex = (int)Math.floor(Math.random() * 2);
+		switch(yourHandIndex) {
+			case 0 -> yourHand = "r";
+			case 1 -> yourHand = "s";
+			case 2 -> yourHand = "p";
+		default -> System.out.println("正しい入力ではありません");
+		}
+		return yourHand;	
+	}
+	
+	public void playGame() {
+		int outcomeIndex;
+		HashMap<String,String> jyanken;
+		System.out.println("自分の手は" + myHand + ", 対戦相手の手は" + yourHand);
+		jyanken = new HashMap<String,String>();
+		
+		jyanken.put("r", "グー");
+		jyanken.put("s", "チョキ");
+		jyanken.put("p", "パー");
+		
+		//じゃんけん結果が正しくないので要修正
+		//勝ちと負けはパターンごとに定義要、あいこはelseで書く
+		//勝ち、負けそれぞれのパターンについては条件内に&& || 使用して書く
+		//outcomeIndex = myHand.compareTo(yourHand);
+		if (myHand == "r"&yourHand == "r" || myHand == "s"&yourHand =="s"||myHand =="p"&yourHand=="p" ) {
+			System.out.println("あいこです");
+		} else if (myHand == "r"&yourHand == "s" || myHand == "s"&yourHand =="p"||myHand =="p"&yourHand=="r" ) {
+				System.out.println("自分の勝ちです");
+		} else if (myHand == "r"&yourHand == "p" || myHand == "s"&yourHand =="r"||myHand =="p"&yourHand=="s" ){
+			System.out.println("自分の負けです");
+		}
+	}
+	
+}
